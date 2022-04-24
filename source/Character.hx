99a8c2758bd5a6102b94c6b14eee01480291c8a5
BrG <102937422+BrG69@users.noreply.github.com>
2022-04-24 20:18:50 +0700

    Add files via upload

--- source/Character.hx
+++ source/Character.hx
@@ -139,6 +139,23 @@ class Character extends FlxSprite
 				addOffset("singDOWN", 0, -30);
 
 				playAnim('idle');
+				case 'noob':
+		       // I suck At Making Cases Lmfao
+				tex = Paths.getSparrowAtlas('characters/Noob', 'shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'Noob Idle', 24);
+				animation.addByPrefix('singUP', 'Noob Up', 24);
+				animation.addByPrefix('singRIGHT', 'Noob Right', 24);
+				animation.addByPrefix('singDOWN', 'Noob Down', 24);
+				animation.addByPrefix('singLEFT', 'Noob Left', 24);
+
+				addOffset('idle');
+				addOffset("singUP", -6, 50);
+				addOffset("singRIGHT", 0, 27);
+				addOffset("singLEFT", -10, 10);
+				addOffset("singDOWN", 0, -30);
+
+				playAnim('idle');
 			case 'spooky':
 				tex = Paths.getSparrowAtlas('characters/spooky_kids_assets');
 				frames = tex;
